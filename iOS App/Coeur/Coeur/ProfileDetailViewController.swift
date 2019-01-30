e5a7188423ef81555568bbcebb94bcf527dfb893
Sally Moon <sallymoon10>
2019-01-30 16:10:08 -0500

    First commit for ProfileDetailViewController

---
+++ iOS App/Coeur/Coeur/ProfileDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ProfileDetailViewController.swift
+//  Coeur
+//
+//  Created by Sally Moon on 2019-01-30.
+//  Copyright © 2019 Coeur. All rights reserved.
+//
+
+import UIKit
+
+class ProfileDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
