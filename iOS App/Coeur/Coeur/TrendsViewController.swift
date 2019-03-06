38f4adcda895bec2ba9d74acb30d92ecc2e8be63
Winnie Fong <winnie_fon@hotmail.com>
2019-03-05 20:28:33 -0500

    added TrendsViewController

---
+++ iOS App/Coeur/Coeur/TrendsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coeur
+//
+//  Created by Mike Zaki on 2019-01-23.
+//  Copyright © 2019 Coeur. All rights reserved.
+//
+
+import UIKit
+
+class TrendsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
